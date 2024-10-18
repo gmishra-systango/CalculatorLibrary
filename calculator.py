6445e9d449606a8e00b3d17dbeab01ecc1bc4a74
Gaurav Mishra <gaurav.mishra@systango.com>
2024-10-18 12:35:19 +0530

    Added Divide functionality with its Unit-tests

--- calculator.py
+++ calculator.py
@@ -14,3 +14,12 @@ def subtract(first_term, second_term):
 # Multiply function
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    try:
+        result = first_term / second_term
+        return result
+    except ZeroDivisionError:
+        print("Division by Zero error")
+        raise
--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,7 @@ Unit tests for the calculator library
 """
 
 import calculator
+import pytest
 
 
 class TestCalculator:
@@ -15,3 +16,10 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+
+        with pytest.raises(ZeroDivisionError):
+            calculator.divide(20, 0)
+
+        assert 5.0 == calculator.divide(20, 4)
